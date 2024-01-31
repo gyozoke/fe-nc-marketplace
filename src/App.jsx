d74fdfaa0168bd24da7a0ea7b66f57c6d62f2ffb
Matt Barker <mattbarker23@hotmail.com>
2024-01-31 12:39:08 +0000

    complete plan outline

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main>
+        <h1>Matt and Victor's shop!</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
